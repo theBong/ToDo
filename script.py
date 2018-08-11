1f2bad426225d3ca33935dddad8e8cdd93931fe8
Ratik Sharma <Ratik96@gmail.com>
2018-08-12 02:02:31 +0530

    Update syntax to match PEP8

--- script.py
+++ script.py
@@ -3,56 +3,50 @@ from items import *
 lis_of_items = {}
 lis_of_todo = {}
 
-def createItem():
-	inpNote = input("What is the note \n")
-	inpDone = bool(input("Done status? \n"))
-	inpID = input("Enter a Item ID\n")
+def create_item():
+	note = input("What is the note? \n")
+	done = bool(input("Done status? \n"))
+	id = input("Enter an item ID\n")
 
-	item = Item(note = inpNote, done = inpDone)
+	item = Item(note=note, done=done)
+	lis_of_items.update({ id : item })
 
-	lis_of_items.update({ inpID : item })
-	return "Created item with ID " + str(inpID)
-
-def createTodo():
-
-	inpName = input("Name the ToDo!\n").title()
-	inpItemsIDs = input("Type out IDs in a line separated by commas, no spaces \n").split(',')
-	inpID = input("Enter a Dictionary ID \n")
-	thinglist = []
-
-	for ID in inpItemsIDs:
-		thinglist.append(lis_of_items[ID])
-
-	ToDo = Todo(name = inpName, things = thinglist)
-	lis_of_todo.update({ inpID : ToDo})
-
-	return "Created  with ID " + str(inpID)
+	return "Created item with ID {}".format(str(id))
 
 
-while (True):
-	
-	inp = int(input('''
-1: Create an Item
-2: Create a ToDo
-Other: Display list of items and ToDos
-'''))
+def create_todo():
+	name = input("Name the ToDo!\n").title()
+	items_ids = input("Type out IDs in a line separated by commas, no spaces \n").split(',')
+	id = input("Enter a Dictionary ID \n")
+	thing_list = []
+
+	for id in items_ids:
+		thing_list.append(lis_of_items[id])
+
+	todo = Todo(name=name, things=thing_list)
+	lis_of_todo.update({ id : todo})
+
+	return "Created with ID {}".format(str(id))
+
+
+while(True):
+	menu = '''
+		1: Create an Item
+		2: Create a ToDo
+		Other: Display list of items and ToDos
+	'''
+	inp = int(input(menu))
 
 	if inp == 1:
 		print("")
-		print(createItem())
-
+		print(create_item())
 	elif inp == 2:
 		print("")
-		print(createTodo())
-
+		print(create_todo())
 	else:
-		print("")
-		print("List of Items: ")
-		for keys, values in lis_of_items.items():
-			print(keys + "> " + str(values))
-		print("")
-		print("List of TodoLists")
-		for keys, values in lis_of_todo.items():
-			print(keys + " = " + str(values))
-
-
+		print("\nList of Items: ")
+		for key, value in lis_of_items.items():
+			print("{}> {}".format(key, value))
+		print("\nList of TodoLists")
+		for key, value in lis_of_todo.items():
+			print("{} = {}".format(key, value))
